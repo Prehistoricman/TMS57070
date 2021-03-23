94f304ce9a7ee4817b7307b5ba26734907c7bfc9
Prehistoricman <ezekieljames@hotmail.com>
2021-03-23 01:51:06 +0000

    Emulator - Add PMEM injection and replacement words, add state report file

--- Emulator/main.cpp
+++ Emulator/main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 constexpr uint32_t PMEM_MAX_WORDS = 0x1FF;
 constexpr uint32_t CMEM_MAX_WORDS = 0x0FF;
+constexpr uint32_t PMEM_INJECT_MAGIC = 0xFEEDBEE5;
 
 TMS57070::Emulator dsp;
 
@@ -31,8 +32,23 @@ static uint32_t ifstream_length(std::ifstream *stream) {
     return (uint32_t)length;
 }
 
-int main() { //int argc, char* argv[]
-    cout << "Hello World!\n";
+int main(int argc, char* argv[]) {
+    uint32_t inject_word = 0;
+    uint32_t replacement_word = 0;
+    uint32_t replacement_pos = UINT32_MAX;
+    if (argc <= 1) {
+        //Nothing
+    } else if (argc == 2) {
+        inject_word = strtoul(argv[1], nullptr, 16);
+    } else if (argc == 4) {
+        inject_word = strtoul(argv[1], nullptr, 16);
+        replacement_word = strtoul(argv[2], nullptr, 16);
+        replacement_pos = strtoul(argv[3], nullptr, 16);
+    } else {
+        printf("Wrong argument count\n");
+        return 1;
+    }
+
 
     //Load dsp.PMEM
     ifstream PMEMFile("D:/Documents/OneDrive/Documents/Digitech XP/Emulator/PMEM.bin", std::ios::binary);
@@ -51,7 +67,13 @@ int main() { //int argc, char* argv[]
     for (uint32_t i = 0; i < pmem_length; i++) {
         PMEMFile.read((char*)readBuffer, 4);
         uint32_t word = readBuffer[0] << 24 | readBuffer[1] << 16 | readBuffer[2] << 8 | readBuffer[3];
-        dsp.PMEM[i] = word;
+        if (word == PMEM_INJECT_MAGIC) {
+            dsp.PMEM[i] = inject_word;
+        } else if (i == replacement_pos) {
+            dsp.PMEM[i] = replacement_word;
+        } else {
+            dsp.PMEM[i] = word;
+        }
     }
 
     for (uint32_t i = 0; i < cmem_length; i++) {
@@ -66,14 +88,23 @@ int main() { //int argc, char* argv[]
     dsp.step();
     dsp.step();
     dsp.sample_in(TMS57070::Channel::in_1L, 0x696969);
-    for (uint16_t i = 0; i < 60; i++) { //TODO: input and output
+    for (uint16_t i = 0; i < 1400; i++) { //TODO: input and output
         dsp.step();
     }
     
 #if 0 //Testing MAC function
     TMS57070::MAC test_mac(nullptr);
-    test_mac.multiply(TMS57070::int24_t{ 0x3FFFFF }, TMS57070::int24_t{ 0x3FFFFF }, TMS57070::signs_t::SS);
-    test_mac.mac(TMS57070::int24_t{ 0x7FFFFF }, TMS57070::int24_t{ 0x800000 }, TMS57070::signs_t::SS);
+    test_mac.multiply(TMS57070::int24_t{ 0x3FFFFF }, TMS57070::int24_t{ 0x3FFFFF }, TMS57070::MACSigns::SS);
+    test_mac.mac(TMS57070::int24_t{ 0x7FFFFF }, TMS57070::int24_t{ 0x800000 }, TMS57070::MACSigns::SS);
 #endif
+
+    //Build state string
+    std::string report = dsp.reportState();
+    //std::cout << report;
+
+    ofstream reportFile("report.txt", std::ios::binary);
+    reportFile.write(report.c_str(), report.size());
+    reportFile.close();
+
     return 0;
 }
